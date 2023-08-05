5262c832a305ae11f85975cba0adb140e82faf21
Andrew Qiao <andrewqiao2004@gmail.com>
2023-08-05 11:24:34 -0400

    add faq section on landing page

--- components/sections/home/FAQ.tsx
+++ components/sections/home/FAQ.tsx
@@ -1,3 +1,45 @@
+import QAndA from '@/components/layout/QAndA';
+
+const QUESTIONS = [
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+];
+
 export default function FAQ() {
-  return <section className='mb-section'></section>;
+  return (
+    <section className='mb-section mx-container grid gap-8 xl:gap-14 xl:grid-cols-[4fr_5fr]'>
+      <h2 className='text-white font-bold text-4xl md:text-8xl xl:text-8xl xl:-mt-6'>
+        Frequently Asked Questions
+      </h2>
+      <QAndA questions={QUESTIONS} />
+    </section>
+  );
 }
