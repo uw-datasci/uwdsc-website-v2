f8d0acc4f9bf9f819e34389017039efa7baf0b7b
ninado <106121863+ndough@users.noreply.github.com>
2023-09-19 20:59:27 -0400

    creating events placeholders

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,1 +1,20 @@
+import React from "react";
+import { Event } from "@/types/types";
 import EventCard from "@/components/cards/EventCard";
+import { UPCOMING_EVENTS } from "@/constants/events";
+import SectionTitle from "@/components/UI/SectionTitle";
+
+export default function Events() {
+  return (
+    <>
+      {UPCOMING_EVENTS.map((event) => (
+        <div key={event.id}>
+          <SectionTitle mb="mb-12">{event.title}</SectionTitle>
+          <div className="flex flex-wrap justify-center gap-8 3xs:gap-12 xl:gap-20">
+            
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
