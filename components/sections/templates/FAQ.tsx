ae271f4efe9826bc3256e57924fda8f1c7ab4794
ninado <106121863+ndough@users.noreply.github.com>
2023-09-27 22:12:57 -0400

    changed format of FAQs

--- components/sections/templates/FAQ.tsx
+++ components/sections/templates/FAQ.tsx
@@ -18,31 +18,29 @@ export default function FAQ({ questions }: FAQProps) {
   };
 
   return (
-    <section className="mb-section mx-container grid gap-8 xl:grid-cols-[4fr_5fr] xl:gap-14">
-      <h2 className="text-4xl font-bold text-white md:text-8xl xl:-mt-6 xl:text-8xl">
+    <section className="mb-section mx-container grid gap-8 xl:grid-cols-[4fr_5fr] xl:gap-14 ">
+      <h2 className="text-4xl font-bold text-white md:text-8xl xl:-mt-6 xl:text-8xl ">
         Frequently Asked Questions
       </h2>
       <div>
         {questions.map((question, i) => (
           <Fragment key={`question-${i}`}>
             <div
-              className={`cursor-pointer ${
-                i === questions.length - 1 ? "" : "pb-7"
-              } ${i === 0 ? "" : "pt-7"}`}
+              className={`cursor-pointer ${i === questions.length - 1 ? "" : "pb-7"
+                } ${i === 0 ? "" : "pt-7"}`}
               onClick={() => toggleHeight(i)}
             >
-              <div className="flex items-center justify-between gap-6">
+              <div className="flex items-center justify-between gap-6 ">
                 <h3 className="font-semibold leading-loose text-white xs:text-lg 2xl:text-xl">
                   {question.question}
                 </h3>
                 <ChevronDown
-                  className={`transition-300 w-6 text-white ${
-                    heights[i] === 0 ? "rotate-0" : "rotate-180"
-                  }`}
+                  className={`transition-300 w-6 text-white ${heights[i] === 0 ? "rotate-0" : "rotate-180"
+                    }`}
                 />
               </div>
               <AnimateHeight duration={300} height={heights[i]}>
-                <p className="mt-4 leading-loose text-grey1 2xl:text-lg">
+                <p className="mt-4 leading-loose text-grey1 2xl:text-lg whitespace-pre w-32 break-words">
                   {question.answer}
                 </p>
               </AnimateHeight>
--- constants/faqs.ts
+++ constants/faqs.ts
@@ -26,32 +26,31 @@ export const GENERAL_FAQ: QandA[] = [
     id: "1",
     question: "What is DSC?",
     answer:
-      "We are a MathSoc club dedicated to building a community of students passionate about exploring the field of Data Science. DSC hosts a number of workshops/events throughout the term, mostly academic focused (see below). Our iconic mascots include Echo the Whale and Bert (ref the ML model by Google)",
+      `We are a MathSoc club dedicated to building a community of students 
+passionate about exploring the field of Data Science. DSC hosts a number
+of workshops/events throughout the term, mostly academic focused (see below). 
+Our iconic mascots include Echo the Whale and Bert (ref the ML model by Google)`,
   },
   {
     id: "2",
     question: "What are some events DSC hosts?",
     answer:
-      `
-    1. Intro workshops on data tools which will look great on your resume :)
-       E.g. Pandas, Tableau, Build Your Own DS Project, Machine Learning Algorithms)
-       
-    2. Industry professionals/alumni/upper-year panels to help students explore more of the field and opportunities
-       E.g. Data Science Across Sectors; intern info sessions with meta, and google; fireside chat w microsoft data scientist)
-    
-    3. Technical interview prep through leetcoding sessions by experienced upper-year mentors and Bootcamp x UW tech clubs (mock interviews + async resume critiques)
-    
-    4. Hackathons to apply your learning and create cool projects 
-       CxC (Data Hackathon) and Project Program (in collab with CSC and LCS)
-    
-    5. Social events to connect with other students
-       Karaoke, scavenger hunt, trivia
-    
-    6. Reading groups targeted to intermediate/advanced members where execs present ML research papers
-       NLP, computer vision
-    
-    7. Visit our BOT (beginning of term event) for more F23 events introductions (details TBA)
-    ` },
+      `1. Intro workshops on data tools which will look great on your resume :) 
+E.g. Pandas, Tableau, Build Your Own DS Project, Machine Learning Algorithms) \n
+2. Industry professionals/alumni/upper-year panels to help students explore more of 
+the field and opportunities
+E.g. Data Science Across Sectors; intern info sessions with meta, and google; 
+fireside chat w microsoft data scientist) \n
+3. Technical interview prep through leetcoding sessions by experienced upper-year 
+mentors and Bootcamp x UW tech clubs (mock interviews + async resume critiques)\n 
+4. Hackathons to apply your learning and create cool projects
+Eg. CxC (Data Hackathon) and Project Program (in collab with CSC and LCS)\n
+5. Social events to connect with other students (Karaoke, scavenger hunt, trivia)\n 
+6. Reading groups targeted to intermediate/advanced members where execs present 
+ML research papers (Eg. NLP, computer vision)\n 
+7. Visit our BOT (beginning of term event) for more F23 events introductions 
+(details TBA)`,
+  },
   {
     id: "3",
     question: "Do I need to major in Data Science?",
@@ -62,20 +61,24 @@ export const GENERAL_FAQ: QandA[] = [
     id: "4",
     question: "How often do I have to go to the events?",
     answer:
-      "All of our events are OPTIONAL! We do NOT have a set schedule for most events, so looking out for the newest info/updates on our Instagram/Discord would be best.",
+      `All of our events are OPTIONAL! We do NOT have a set schedule for most events, 
+so looking out for the newest info/updates on our Instagram/Discord would be best.`,
   },
   {
     id: "5",
     question: "What is the $4 membership fee going to?",
     answer:
-      "The membership fee covers the ENTIRE Fall2023 term (not per event). By paying the membership fee, you can attend all of our events, most of which include FREE FOOD.",
+      `The membership fee covers the ENTIRE Fall2023 term (not per event). By paying 
+the membership fee, you can attend all of our events, most of which include 
+FREE FOOD.`,
   },
   {
     id: "6",
     question: "How to sign up?",
     answer:
-    `
-    Scan QR code at the booth or go to our LinkTree (in Instagram bio) to access the MEMBER SIGN-UP form. You can pay the $4 membership fee by one of the following methods:
+    `Scan QR code at the booth or go to our LinkTree (in Instagram bio) to access the 
+MEMBER SIGN-UP form. You can pay the $4 membership fee by one of the 
+following methods:
     
     - Online through WUSA (follow steps on the sign-up form description)
     - In-person at our office/events with cash
