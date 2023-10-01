76c6ad68304d68675bf881839ec724b70839c57b
Andrew Qiao <andrewqiao2004@gmail.com>
2023-10-01 18:41:51 -0400

    reformat faq text

--- components/sections/templates/FAQ.tsx
+++ components/sections/templates/FAQ.tsx
@@ -23,35 +23,41 @@ export default function FAQ({ questions }: FAQProps) {
         Frequently Asked Questions
       </h2>
       <div>
-        {questions.map((question, i) => (
-          <Fragment key={`question-${i}`}>
-            <div
-              className={`cursor-pointer ${
-                i === questions.length - 1 ? "" : "pb-7"
-              } ${i === 0 ? "" : "pt-7"}`}
-              onClick={() => toggleHeight(i)}
-            >
-              <div className="flex items-center justify-between gap-6 ">
-                <h3 className="font-semibold leading-loose text-white xs:text-lg 2xl:text-xl">
-                  {question.question}
-                </h3>
-                <ChevronDown
-                  className={`transition-300 w-6 text-white ${
-                    heights[i] === 0 ? "rotate-0" : "rotate-180"
-                  }`}
-                />
+        {questions.map((question, i) => {
+          const lines = question.answer.split("\n").map((line, i) => (
+            <p className="mt-4 leading-loose text-grey1 2xl:text-lg" key={i}>
+              {line}
+            </p>
+          ));
+
+          return (
+            <Fragment key={`question-${i}`}>
+              <div
+                className={`cursor-pointer ${
+                  i === questions.length - 1 ? "" : "pb-5 2xl:pb-7"
+                } ${i === 0 ? "" : "pt-5 2xl:pt-7"}`}
+                onClick={() => toggleHeight(i)}
+              >
+                <div className="flex items-center justify-between gap-6 ">
+                  <h3 className="font-semibold leading-loose text-white xs:text-lg 2xl:text-xl">
+                    {question.question}
+                  </h3>
+                  <ChevronDown
+                    className={`transition-300 w-6 text-white ${
+                      heights[i] === 0 ? "rotate-0" : "rotate-180"
+                    }`}
+                  />
+                </div>
+                <AnimateHeight duration={300} height={heights[i]}>
+                  {lines}
+                </AnimateHeight>
               </div>
-              <AnimateHeight duration={300} height={heights[i]}>
-                <p className="mt-4 w-32 whitespace-pre break-words leading-loose text-grey1 2xl:text-lg">
-                  {question.answer}
-                </p>
-              </AnimateHeight>
-            </div>
-            {i !== questions.length - 1 && (
-              <hr className="border-b-1 border-grey3" />
-            )}
-          </Fragment>
-        ))}
+              {i !== questions.length - 1 && (
+                <hr className="border-b-1 border-grey3" />
+              )}
+            </Fragment>
+          );
+        })}
       </div>
     </section>
   );
