8cf5ec63c64efb27d40da66b0596f8b5b1b336bd
ninado <106121863+ndough@users.noreply.github.com>
2023-11-07 14:10:17 -0500

    Update upcoming events

--- constants/events.ts
+++ constants/events.ts
@@ -3,29 +3,11 @@ import { Event } from "@/types/types";
 import event from "@/public/placeholder/event.png";
 import system from "@/public/events/system.png";
 import reading from "@/public/events/reading.png";
-import regress from "@/public/events/regress.png";
+import workshop from "@/public/events/workshop.png";
 
 export const UPCOMING_EVENTS: Event[] = [
   {
     id: "0",
-    title: "Regress vs Classify",
-    description:
-      "Unlock the Power of Regression and Classification: A Workshop for Predictive and Exploratory Analytics!",
-    image: regress,
-    date: "6-7:30pm, Thurs, Nov 2",
-    location: "MC 5501",
-  },
-  {
-    id: "1",
-    title: "Reading Group",
-    description:
-      "This week's reading group is about gaming and reinforcement learnings! The link is available on our instagram!",
-    image: reading,
-    date: "6-7pm, Sunday, Nov 5",
-    location: "zoom",
-  },
-  {
-    id: "2",
     title: "Info + Networking",
     description:
       "Join us for an informative and networking session with Point 72 - Cubist Systematic Strategies!",
@@ -33,37 +15,55 @@ export const UPCOMING_EVENTS: Event[] = [
     date: "6-7pm, Wed, Nov 8",
     location: "STC 0050",
   },
-];
-
-export const PAST_EVENTS: Event[] = [
   {
     id: "1",
-    title: "E-Leetcoding",
-    image: event,
+    title: "Reinforcement",
+    description:
+      "Get ready to build your own intelligent agent and  simulate your RL agent using Pytorch and OpenAI's Gym! ",
+    image: workshop,
+    date: "6:30-8pm, Thurs, Nov 9",
+    location: "MC 5501",
   },
   {
     id: "2",
-    title: "E-Leetcoding",
-    image: event,
-  },
-  {
-    id: "3",
-    title: "E-Leetcoding",
-    image: event,
-  },
-  {
-    id: "4",
-    title: "E-Leetcoding",
-    image: event,
-  },
-  {
-    id: "5",
-    title: "E-Leetcoding",
-    image: event,
-  },
-  {
-    id: "6",
-    title: "E-Leetcoding",
-    image: event,
+    title: "Reading Group",
+    description:
+      "This reading group is going to cover topics about diffusion models! The link is available on our instagram!",
+    image: reading,
+    date: "6-7pm, Sunday, Nov 12",
+    location: "zoom",
   },
 ];
+
+// export const PAST_EVENTS: Event[] = [
+//   {
+//     id: "1",
+//     title: "E-Leetcoding",
+//     image: event,
+//   },
+//   {
+//     id: "2",
+//     title: "E-Leetcoding",
+//     image: event,
+//   },
+//   {
+//     id: "3",
+//     title: "E-Leetcoding",
+//     image: event,
+//   },
+//   {
+//     id: "4",
+//     title: "E-Leetcoding",
+//     image: event,
+//   },
+//   {
+//     id: "5",
+//     title: "E-Leetcoding",
+//     image: event,
+//   },
+//   {
+//     id: "6",
+//     title: "E-Leetcoding",
+//     image: event,
+//   },
+// ];
