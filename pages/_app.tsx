7ed873a0eb245a58dcaa38e056f2a41e06cbd01e
Sheng Liang <shengliangchow@gmail.com>
2024-07-24 18:28:01 -0400

    moved SignUp component to doc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import ReduxProvider from "@/components/redux/ReduxProvider";
 import Navbar from "@/components/navigation/Navbar";
 import Footer from "@/components/navigation/Footer";
+import SignUp from "@/components/sections/home/SignUp";
 
 import "@/styles/globals.css";
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider>
       <Navbar />
+      <SignUp/>
       <Component {...pageProps} />
       <Footer />
     </ReduxProvider>
