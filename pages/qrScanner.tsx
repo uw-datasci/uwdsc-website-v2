86389f7b5905dcc445c1a4f9602d78e0db375ca5
xinhuey <xinhuey.wong@gmail.com>
2024-09-17 18:25:26 -0400

    cleaned up code

--- pages/qrScanner.tsx
+++ pages/qrScanner.tsx
@@ -15,8 +15,7 @@ const qrScanner = () => {
   const onScanSuccess = (result: QrScanner.ScanResult) => {
       // 🖨 Print the "result" to browser console.
       console.log(result);
-      // ✅ Handle success.
-      // 😎 You can do whatever you want with the scanned result.
+  
       setScannedResult(result?.data);
   };
 
@@ -28,20 +27,20 @@ const qrScanner = () => {
 
   useEffect(() => {
       if (videoEl?.current && !scanner.current) {
-          // 👉 Instantiate the QR Scanner
+
           scanner.current = new QrScanner(videoEl?.current, onScanSuccess, {
               onDecodeError: onScanFail,
-              // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
+  
               preferredCamera: "environment",
-              // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
+              
               highlightScanRegion: true,
-              // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
+              // This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
               highlightCodeOutline: true,
-              // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
+              // A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
               overlay: qrBoxEl?.current || undefined,
           });
 
-          // 🚀 Start QR Scanner
+          // Start QR Scanner
           scanner?.current
               ?.start()
               .then(() => setQrOn(true))
@@ -50,8 +49,8 @@ const qrScanner = () => {
               });
       }
 
-      // 🧹 Clean up on unmount.
-      // 🚨 This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
+      // Clean up on unmount.
+      // This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
       return () => {
           if (!videoEl?.current) {
               scanner?.current?.stop();
@@ -59,7 +58,7 @@ const qrScanner = () => {
       };
   }, []);
 
-  // ❌ If "camera" is not allowed in browser permissions, show an alert.
+  // If "camera" is not allowed in browser permissions, show an alert.
   useEffect(() => {
       if (!qrOn)
           alert(
