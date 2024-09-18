546d5cb078f79887d72a5947c305ddba749b7d50
applepie7864 <anniewenqingguo@gmail.com>
2024-09-17 20:33:56 -0400

    encrypt the qr code data

--- pages/api/send/qr-code.ts
+++ pages/api/send/qr-code.ts
@@ -1,13 +1,15 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
-import axios from "axios";
 require('dotenv').config()
 const qr = require("qrcode");
+const bcrypt = require("bcryptjs");
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const dataString = JSON.stringify(req.body)
+  const id = await bcrypt.hash(req.body.id, 10);
+  const event = await bcrypt.hash(req.body.event, 10);
+  const dataString = JSON.stringify({ id, event })
   const image = await qr.toDataURL(dataString)
   res.status(200).json({ success: true, dataUrl: image }); // accessToken: response.data.accessToken ???
 }
