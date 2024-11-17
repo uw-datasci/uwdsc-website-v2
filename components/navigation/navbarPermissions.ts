916022a8fbd54e69f293c59f99b51e40146660a1
Mahdi Raza Khunt <82615934+mrkhunt@users.noreply.github.com>
2024-11-16 19:13:19 -0500

    feat: add navbar routes

---
+++ components/navigation/navbarPermissions.ts
@@ -0,0 +1,26 @@
+export const NAVBAR_ROUTES = {
+  NOT_LOGGED_IN: [
+    { label: "Home", route: "/" },
+    { label: "CxC", route: "/cxc" },
+    { label: "Team", route: "/team" },
+    { label: "Contact", route: "/#contact" },
+  ],
+  USER: [
+    { label: "Home", route: "/" },
+    { label: "CxC", route: "/cxc" },
+    { label: "Team", route: "/team" },
+    { label: "QR Code", route: "/qrPage" },
+  ],
+  ADMIN: [
+    { label: "Home", route: "/" },
+    { label: "CxC", route: "/cxc" },
+    { label: "Team", route: "/team" },
+    {
+      label: "Admin",
+      subNavItems: [
+        { label: "Memberships", route: "/admin" },
+        { label: "QR Scanner", route: "/qrScanner" },
+      ],
+    },
+  ],
+};
