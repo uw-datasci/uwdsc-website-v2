7e8d47d0cc004e57f489ad0fa12e47880360ddef
winstonyou <78757258+winstonyou@users.noreply.github.com>
2025-01-18 14:30:48 -0500

    removed waterloo email part

--- constants/forms.ts
+++ constants/forms.ts
@@ -136,7 +136,7 @@ export const FORGOT_PASSWORD_FORM_FIELDS: ContactField[] = [
     id: "email",
     name: "email",
     type: "input",
-    placeholder: "Waterloo Email (ex. slchow@uwaterloo.ca)",
+    placeholder: "Email (ex. slchow@uwaterloo.ca)",
   },
 ];
 
