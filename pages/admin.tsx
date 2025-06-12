af399be650214689ca1bbf54428fb93b180e1697
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-06-11 20:49:17 -0400

    Add exec/admin routes

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,16 +1,19 @@
 import SectionTitle from "@/components/UI/SectionTitle";
 import withAuth from "@/components/permissions/authPage";
 import AdminTable from "@/components/tables/AdminTable";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 require("dotenv").config();
 
 function Admin() {
+  const userRole = useSelector((state: RootState) => state.loginToken.role);
 
   return (
     <>
       <section className="mx-container mb-section mt-14 lg:mt-20">
         <h1 className="mb-14 text-center text-3xl font-bold text-white 3xs:text-6xl sm:text-8xl lg:text-10xl 2xl:text-12xl">
-          Admin
+          {userRole === "admin" ? "Admin Dashboard" : "Exec Dashboard"}
         </h1>
         <div className="flex flex-col gap-2 overflow-visible">
           <SectionTitle mb="mb-12">Memberships</SectionTitle>
@@ -21,4 +24,4 @@ function Admin() {
   );
 }
 
-export default withAuth(Admin, ["admin"]);
+export default withAuth(Admin, ["admin", "exec"]);
