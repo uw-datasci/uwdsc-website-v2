0cbfde43514e490dec10c643671a793f819c0289
Aaryan Patel <a52patel@uwaterloo.ca>
2026-01-14 15:18:12 -0500

    Update team.ts

--- constants/team.ts
+++ constants/team.ts
@@ -13,6 +13,7 @@ import aditiJha from "@/public/teams/Aditi-Jha.jpg";
 import shreeyaSantha from "@/public/teams/Shreeya-Santha.jpg";
 import brianAn from "@/public/teams/Brian-An.jpg";
 import andyXu from "@/public/teams/Andy-Xu.jpg";
+import jessicaYing from "@/public/teams/Jessica-Ying.jpg";
 import andrewChu from "@/public/teams/Andrew-Chu.jpg";
 import samayBhagat from "@/public/teams/Samay-Bhagat.jpg";
 
@@ -213,7 +214,7 @@ export const TEAM: Subteam[] = [
         id: "jessica-ying",
         name: "Jessica Ying",
         position: "Developer",
-        image: team,
+        image: jessicaYing,
       },
       {
         id: "sophie-yang",
